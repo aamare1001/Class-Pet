f545db3f059ece732a83467249772e0577220301
Amare <aamare100@gmail.com>
2020-11-28 17:17:13 -0500

    Testing out things Added Chat DM & Uptime commands

--- my_bot.js
+++ my_bot.js
@@ -13,11 +13,31 @@ client.on('message', message =>
 
     const args = message.content.slice(config.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
-    var name = message.author.display_name;
+    var name = message.author.username;
 
     if(command === "ping")
     {
-        message.channel.send("pong! " + message.author.display_name);
+        message.channel.send("pong! " + message.author.username);
+        console.log("ponged in " + message.channel.name);
+    }
+
+    if(command === "chat")
+    {
+        //const chat = new Discord.DMChannel(client, message.author);
+        console.log("started chat with " + name);
+        message.author.send("hi");
+        // const filter = m => m.content.includes('discord');
+        // const collector = chat.createMessageCollector(filter, { time: 15000 });
+        // collector.on('collect', m => console.log(`Collected ${m.content}`));
+        // chat.send("hi there!");
+        // collector.on('end', collected => console.log(`Collected ${collected.size} items`))
+    }
+    if(command === "up")
+    {
+        var time = client.uptime / 1000;
+        time = Math.floor(time / 60);
+        message.channel.send("I've been up for: " + time +" minutes");
+        console.log("I've been up for: %d minutes", time);
     }
 })
 
